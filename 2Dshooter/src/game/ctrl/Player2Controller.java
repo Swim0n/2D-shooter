06a6ad51457aa5a936909411977e2fb7871a1ea4
Swim0n <simon.y.mare@gmail.com>
2016-04-26 15:18:07 +0200

    added some files i forgot to the player 2-update...

---
+++ 2Dshooter/src/game/ctrl/Player2Controller.java
@@ -0,0 +1,41 @@
+package game.ctrl;
+
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+import game.gameView.PlayerView;
+
+/**
+ * Created by Simon on 2016-04-26.
+ */
+public class Player2Controller extends PlayerController implements ActionListener {
+
+    public Player2Controller(PlayerView view, float radius, float height, float mass) {
+        super(view,radius,height,mass);
+        setupKeys();
+    }
+
+    private void setupKeys() {
+        inputManager.addMapping("left2", new KeyTrigger(KeyInput.KEY_A));
+        inputManager.addMapping("right2", new KeyTrigger(KeyInput.KEY_D));
+        inputManager.addMapping("up2", new KeyTrigger(KeyInput.KEY_W));
+        inputManager.addMapping("down2", new KeyTrigger(KeyInput.KEY_S));
+        inputManager.addListener(this, "left2");
+        inputManager.addListener(this, "right2");
+        inputManager.addListener(this, "up2");
+        inputManager.addListener(this, "down2");
+    }
+
+    public void onAction(String name, boolean isPressed, float tpf) {
+        //movement of player
+        if (name.equals("left2")) {
+            left = isPressed;
+        } else if (name.equals("right2")) {
+            right = isPressed;
+        } else if (name.equals("up2")) {
+            up = isPressed;
+        } else if (name.equals("down2")) {
+            down = isPressed;
+        }
+    }
+}
