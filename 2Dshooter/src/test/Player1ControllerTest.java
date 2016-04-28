d3f8145a4fb70c2493d10f2d79079741b11e5d71
Hannes Thell <hannes.thell@live.se>
2016-04-28 10:59:38 +0200

    started with some testing.

---
+++ 2Dshooter/src/test/Player1ControllerTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Hannes on 26/04/2016.
+ */
+public class Player1ControllerTest {
+
+    @org.junit.Test
+    public void onAction() throws Exception {
+
+    }
+}
