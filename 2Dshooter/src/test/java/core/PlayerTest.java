aaf0af58524a0ebd65bf902c23031668cd07df22
Hannes Thell <hannes.thell@live.se>
2016-05-24 17:13:58 +0200

    first test on power up.

---
+++ 2Dshooter/src/test/java/core/PlayerTest.java
@@ -0,0 +1,31 @@
+package core;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Hannes on 24/05/2016.
+ */
+public class PlayerTest {
+    @Test
+    public void setHealth() throws Exception {
+
+    }
+
+    @Test
+    public void getHealth() throws Exception {
+
+    }
+
+    @Test
+    public void getPosition() throws Exception {
+
+    }
+
+    @Test
+    public void setPosition() throws Exception {
+
+    }
+
+}
