ed72f6948387cd8fcdd7a19c1f13a2eb4f3b0040
David Iliefski <dij@live.se>
2016-05-29 03:49:45 +0200

    Minor merge fixes

--- 2Dshooter/src/main/java/Main.java
+++ 2Dshooter/src/main/java/Main.java
@@ -4,7 +4,7 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         ViewInit viewInit = new ViewInit();
-        if(viewInit.getGameView().getWorld().isShutDown()){return;}
+        if(viewInit.getWorldView().getWorld().isShutDown()){return;}
         new ControlInit(viewInit);
     }
 }
